bd03061f95efd25f278f5695d474719c7d920b2d
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-08-08 11:57:42 +0530

    setup stored

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -10,3 +10,5 @@ function App() {
 }
 
 export default App;
+
+//store setup -> app.js(store) -> news()
---
+++ newsapp/src/redux/newsReducer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const newsreducer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default newsreducer
